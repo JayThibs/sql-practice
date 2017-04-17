0d4b955050cd9f2d5d67a35663858e3bd5dbbe8c
JayThibs <thibo.jacques@gmail.com>
2017-04-17 15:00:29 -0300

    section 2 tutorial complete

---
+++ Section-2-Problems-SELECT-from-WORLD.sql
@@ -0,0 +1,93 @@
+-- SELECT from WORLD Tutorial - Section 2 of SQLZoo.net
+
+-- 1. Show the name, continent and population of all countries.
+
+SELECT name, continent, population FROM world
+
+-- 2. Show the name for the countries that have a population 
+-- of at least 200 million.
+
+SELECT name FROM world
+WHERE population > 200000000
+
+-- 3. Give the name and the per capita GDP for those countries 
+-- with a population of at least 200 million.
+
+SELECT name, gdp/population
+FROM world
+WHERE population > 200000000
+
+-- 4. Show the name and population in millions for the countries 
+-- of the continent 'South America'. Divide the population by 
+-- 1000000 to get population in millions.
+
+SELECT name, population/1000000
+FROM world
+WHERE continent IN ('South America')
+
+-- 5. Show the name and population for France, Germany, Italy
+
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+-- 6. Show the countries which have a name that includes the word 'United'
+
+SELECT name
+FROM world
+WHERE name LIKE '%UNITED%'
+
+-- 7. Two ways to be big: A country is big if it has an area of more than 
+-- 3 million sq km or it has a population of more than 250 million.
+-- Show the countries that are big by area or big by population. 
+-- Show name, population and area.
+
+SELECT name, population, area
+FROM world
+WHERE area > 3E6 OR population > 250E6
+
+-- 8. Exclusive OR (XOR). Show the countries that are big by area or 
+-- big by population but not both. Show name, population and area.
+
+SELECT name, population, area
+FROM world
+WHERE area > 3E6 XOR population > 250E6
+
+-- 9. For South America show population in millions and 
+-- GDP in billions both to 2 decimal places.
+
+SELECT name, ROUND(population/(1E6), 2), ROUND(gdp/(1E9), 2)
+FROM world
+WHERE continent IN ('South America')
+
+-- 10. Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp > 1E12
+
+-- 11. Show the name and capital where the name and 
+-- the capital have the same number of characters.
+
+SELECT name, capital
+  FROM world
+ WHERE LENGTH(name) = LENGTH(capital)
+ 
+ -- 12. Show the name and the capital where the first letters 
+ -- of each match. Don't include countries where the name 
+ -- and the capital are the same word.
+ 
+ SELECT name, capital
+FROM world
+WHERE name <> capital AND LEFT(name,1) = LEFT(capital,1)
+
+-- 13. Find the country that has all the vowels and no spaces in its name.
+
+SELECT name
+   FROM world
+WHERE name LIKE '%A%'
+  AND name LIKE '%E%'
+  AND name LIKE '%I%'
+  AND name LIKE '%O%'
+  AND name LIKE '%U%'
+  AND name NOT LIKE '% %'
